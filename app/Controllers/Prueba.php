82bc4f737f8c7daf805c3db6d091e36645f62d0a
federico <federic47@hotmail.com>
2019-10-25 18:42:53 +0000

    primer commit

---
+++ app/Controllers/Prueba.php
@@ -0,0 +1,19 @@
+<?php namespace App\Controllers;
+
+class Prueba extends BaseController
+{
+	public function index()
+	{
+		echo "esta es la funcion index";
+	}
+   public function test(){
+     echo "esta es la funcion test";
+
+	 }
+	 public function test2(){
+     echo "esta es la funcion test2";
+
+	 }
+	//--------------------------------------------------------------------
+
+}
